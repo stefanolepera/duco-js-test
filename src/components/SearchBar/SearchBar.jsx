89d770440b2e09508f32814d60f2beb6dde67cd7
Stefano Le Pera <stefano.lepera@gamesys.co.uk>
2018-12-06 01:14:07 +0000

    added axios for fetching data, added some logic to SearchBar and AppContainer

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,5 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const SearchBar = () => <div />;
+class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            character: ''
+        };
+    }
+
+    onChange = e => {
+        this.setState({ [e.target.name]: e.target.value }, () => {
+            this.props.handleOnChange(this.state.character);
+        });
+    };
+
+    render() {
+        const { character } = this.state;
+        return (
+            <input
+                type="text"
+                name="character"
+                placeholder="Search Character"
+                onChange={this.onChange}
+                value={character}
+                autoFocus
+            />
+        );
+    }
+}
 
 export default SearchBar;
--- src/container/AppContainer.jsx
+++ src/container/AppContainer.jsx
@@ -1,9 +1,38 @@
 import React, { Component } from 'react';
+import Axios from 'axios';
 import SearchBar from '../components/SearchBar/SearchBar';
 
 class AppContainer extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            query: '',
+            characters: []
+        };
+    }
+
+    handleOnChange = value => {
+        this.setState({ query: value }, () => {
+            this.getInfo();
+        });
+    };
+
+    getInfo = () => {
+        Axios.get(`https://swapi.co/api/people/?search=${this.state.query}`)
+            .then(res => {
+                console.log('res.data.results', res.data.results);
+                this.setState({
+                    characters: res.data.results
+                });
+            })
+            .catch(err => {
+                console.log('err', err);
+            });
+    };
+
     render() {
-        return <SearchBar />;
+        return <SearchBar handleOnChange={this.handleOnChange} />;
     }
 }
 
