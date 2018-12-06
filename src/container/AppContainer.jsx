1e410926a0efcebb567b2bcb42a889abfa2f4939
Stefano Le Pera <stefano.lepera@gamesys.co.uk>
2018-12-06 00:49:14 +0000

    base folder structure and some boilerplate cleanup

---
+++ src/container/AppContainer.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import SearchBar from '../components/SearchBar/SearchBar';
+
+class AppContainer extends Component {
+    render() {
+        return <SearchBar />;
+    }
+}
+
+export default AppContainer;
